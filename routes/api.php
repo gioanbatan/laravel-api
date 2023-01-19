3e25afb9380bfefce7c9b8c75f27e6fe64a8fcb5
gioanbatan <giovannibassani@hotmail.com>
2023-01-19 15:34:47 +0100

    Add: API send projects with type and technologies (milestone 1)

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,3 +25,5 @@ Route::get('/test', function () {
         'response' => 'CIAO'
     ]);
 });
+
+Route::get('projects', [ProjectController::class, 'index']);
